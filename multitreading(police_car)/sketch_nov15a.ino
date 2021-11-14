e526bf456a7a30a6bd5e27a8decf39dcd082ec41
elisialight <elisialight@gmail.com>
2021-11-15 02:12:30 +0300

    need to make buzzer as multithread function

--- multitreading(police_car)/sketch_nov15a.ino
+++ multitreading(police_car)/sketch_nov15a.ino
@@ -1,3 +1,4 @@
+#include <Thread.h>
 const int IN1 = 2;
 const int IN2 = 3;
 const int ENA = 4;
@@ -10,10 +11,17 @@ int const allMotors[] = {IN1, IN2, IN3, IN4};
 int input=0;
 int buzz=13; // Buzzer Pin
 
+Thread buzzerThread = Thread();
+Thread ledThread = Thread();
 void setup() {
   for(int i=2;i<=12;i++){
     pinMode(i,OUTPUT);
   }
+  ledThread.onRun(led);
+  ledThread.setInterval(100);
+
+  buzzerThread.onRun(buzzer);
+  buzzerThread.setInterval(5);
 }
 
 /*boolean debounce(boolean last){                 //Function to solve the problem of button debouncing
@@ -27,10 +35,13 @@ void setup() {
 } */
   
 void loop() {
-  for(int i=3;i<=12;i++){
-    digitalWrite(i,LOW);
-    one();
-  } 
+  if (ledThread.shouldRun()) {
+    ledThread.run();
+  }
+
+  if (buzzerThread.shouldRun()) {
+    buzzerThread.run();  
+  }
 }
 
 void forward() {
@@ -40,32 +51,46 @@ void forward() {
 }
 
 void led() {
-  for (int i = 0; i < sizeOfLeds; i++) {
+  static int i = 0;
+  static int step = 0;
+  if (step == 0) { 
     digitalWrite(leds[i], HIGH);
-    delay(100);
+    step = 1;
+  }else if (step == 1) {
     digitalWrite(leds[(i + 1) % sizeOfLeds], HIGH);
-    delay(100);
+    step = 2;
+  }else {
     digitalWrite(leds[i], LOW);
+    step = 0;
+    i= (i+1) % sizeOfLeds;
+  }
+  // delay
+}
+
+void buzzerUp() {
+    for(int hz = 440; hz < 1000; hz+=25){
+      tone(buzz, hz, 50);
+      //delay 5
+    }
+}
+
+void buzzerDown() {
+  for(int hz = 1000; hz > 440; hz-=25){
+    tone(buzz, hz, 50);
+    //delay 5
   }
 }
 
-void one() {         //This function produces the 1st siren sound with ON/OFF led transition.
+void buzzer() {         //This function produces the 1st siren sound with ON/OFF led transition.
   // Whoop up
+  
   for(int hz = 440; hz < 1000; hz+=25){
     tone(buzz, hz, 50);
     delay(5);
-    for(int i=3;i<=7;i++) {
-      digitalWrite(i,HIGH);
-    }
   }
   // Whoop down
   for(int hz = 1000; hz > 440; hz-=25){
     tone(buzz, hz, 50);
     delay(5);
-    for(int i=3;i<=7;i++)
-    {
-      digitalWrite(i,LOW);
-      digitalWrite(i+5,HIGH);
-    }
  }
 }
