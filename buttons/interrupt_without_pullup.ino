e32eb4b3c8a25095feeb49ba1ab2da182c9a0f32
elisialight <elisialight@gmail.com>
2021-11-25 20:44:12 +0300

    add led

--- buttons/interrupt_without_pullup.ino
+++ buttons/interrupt_without_pullup.ino
@@ -1,18 +1,20 @@
-bool isPulled = false;
+int isPulled = 0;
 void setup() {
   // прерывание на D2 (UNO/NANO)
-  attachInterrupt(0, isr, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(2), myButt, CHANGE);
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
   //nMode(2, INPUT_PULLUP);
 }
 
 volatile uint32_t debounce;
-void isr() {
+void myButt() {
 
   if (millis() - debounce >= 100 && digitalRead(2)) {
     debounce = millis();
 	Serial.println(isPulled);
     isPulled = !isPulled;
+    digitalWrite(13, isPulled);
   }
 }
 void loop() {
