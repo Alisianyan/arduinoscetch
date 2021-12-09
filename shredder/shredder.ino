4f37edced70c0608afad13984bce362d33ba4687
elisialight <elisialight@gmail.com>
2021-12-10 02:40:58 +0300

    using if ver 1

--- shredder/shredder.ino
+++ shredder/shredder.ino
@@ -1,30 +1,73 @@
-void turnOnMotor() {
-	if (millis() - timeOfLastPull >= debounce && digitalRead(2)) {
-		timeOfLastPull = millis();
-		digitalWrite(enginePin, HIGH);
-	}
-	
-	if (millis() - lastTimeAskingEngine > engineAskTime && analogRead(A0) < idleLevel) {
-		lastTimeAskingEngine = millis();
-		digitalWrite(enginePin, LOW);
-?		isForward = true;		
-	}
-	
-	if (millis() - blockedTime > turnBackwardInterval && analogRead(A0) < shortCurcuitLevel) {
-		blockedTime = millis();
-		digitalWrite(enginePin, LOW);
-		digitalWrite(relePin, HIGH);
-		timeWhenRelayChanged = millis();
-		isForward = false;
-		if (millis() - timeWhenRelayChanged > turnRelayInterval && analogRead(A0) ){
-			digitalWrite(enginePin, HIGH);
-		}
-	}
-	
-	if (millis() - unblockedTime > turnBackwardInterval && analogRead(A0) < idleLevel && isForward != true) {
-		unblockedTime = millis();
-		digitalWrite(enginePin, LOW);
-		digitalWrite(relePin, LOW);
-	}
+#define debounce 50
+#define enginePin 6
+#define relayPin 7
+#define idleLevel 100
+#define SClevel 800
+
+unsigned long timeOfSCBegin = 0;
+unsigned long relayTime = 0;
+unsigned long timeOfLastPull = 0;
+unsigned long timeOfLastAskIdle = 0;
+unsigned long timeOfLastAskSC = 0;
+bool isForward = true;
+
+void setup() {
+  pinMode(enginePin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
+  Serial.begin(9600);
 }
 
+void loop() {
+  turnOnMotor();
+}
+
+void turnOnMotor() {
+  if ((unsigned long)(millis() - timeOfLastPull) >= debounce && digitalRead(2) == HIGH && isForward == true) {    
+    timeOfLastPull = millis();
+    digitalWrite(enginePin, HIGH);
+  }
+
+  if ((unsigned long)(millis() - timeOfLastAskIdle) > 100) {
+    timeOfLastAskIdle=millis();
+    if (analogRead(A0) < idleLevel) {
+      if (isForward) {
+        digitalWrite(enginePin, LOW);
+      }else {
+        digitalWrite(enginePin, LOW);
+        digitalWrite(relayPin, LOW);
+        isForward = true;
+      }
+    }     
+  }
+  
+  if ( (unsigned long)(millis() - timeOfLastAskSC) > 100) {
+    timeOfLastAskSC = millis();
+    if (analogRead(A0) > SClevel) {
+      if (timeOfSCBegin == 0) {
+        timeOfSCBegin = millis();
+      }
+    }else {
+      timeOfSCBegin = 0;
+    }
+  }
+  
+  
+  if (timeOfSCBegin != 0 && (unsigned long)(millis() - timeOfSCBegin) > 2000) {
+    if (isForward) {
+      digitalWrite(enginePin, LOW);
+      digitalWrite(relayPin, HIGH);
+      timeOfSCBegin = 0;
+      relayTime = millis();
+      isForward = false;
+    }else {
+      // что делать, если заклинило на пкти назад
+      
+    }
+  }
+  
+  if (relayTime != 0 && (unsigned long)(millis() - relayTime) > 400) {
+    digitalWrite(enginePin, HIGH);
+    relayTime = 0;
+  }
+  Serial.println(analogRead(A0));
+}
