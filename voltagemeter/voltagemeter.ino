7a2ca20cf92027e59c77be4e5350a130f4769a33
elisialight <elisialight@gmail.com>
2023-10-09 20:00:59 +0300

    voltmeter and ws2812

--- voltagemeter/voltagemeter.ino
+++ voltagemeter/voltagemeter.ino
@@ -1,17 +1,36 @@
 int sensorValue = 0;
 float voltage = 0;
+#include <Adafruit_NeoPixel.h>
+#define PIN 3   // input pin Neopixel is attached to
+#define NUMPIXELS      12 // number of neopixels in strip
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 void setup()
 {
   pinMode(A0, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  pixels.begin();
 }
 
 void loop()
 {
 
+  voltmeter();
+  Serial.println(voltmeter());
+  led_on(int());
+}
+
+float voltmeter(){
   sensorValue = analogRead(A0);
   voltage = sensorValue*5/1023;
-  Serial.println(voltage);
-  delay(100);
+  return voltage;
 }
+
+void led_on(int num_leds){
+    for (int i=0; i < num_leds; i=i+1) {
+     pixels.setPixelColor(i, pixels.Color(250, 250, 0));
+     pixels.show();
+    delay(300);
+  }
+  }
+
+  
